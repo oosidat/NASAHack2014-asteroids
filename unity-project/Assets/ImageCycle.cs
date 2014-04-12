39e21ca21c4f2562b12fcac33b2a3ad36da0583d
macbook <macbook@wwwerd>
2014-04-12 19:09:42 -0400

    added hud and graphs

---
+++ unity-project/Assets/ImageCycle.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ImageCycle : MonoBehaviour {
+
+	public Texture2D[] images;
+	int image_num = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.RightArrow))
+		    {
+				image_num++;
+						if (image_num >= images.Length) {
+							image_num = 0;		
+						}
+				renderer.material.mainTexture = images[image_num];
+			}
+
+		if (Input.GetKeyDown (KeyCode.LeftArrow))
+		{
+			image_num--;	
+						if (image_num < 0) {
+							image_num = 12;		
+						}
+			renderer.material.mainTexture = images[image_num];
+		}
+
+
+	}
+}
