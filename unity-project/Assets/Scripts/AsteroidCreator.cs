693272967b9cb69f8e5c89919f7c9eff877c8836
Osama Sidat <oosidat@gmail.com>
2014-04-13 00:27:38 -0400

    Refactor, added for loop to instantiate 10 asteroids at random positions in osamascene

--- unity-project/Assets/Scripts/AsteroidCreator.cs
+++ unity-project/Assets/Scripts/AsteroidCreator.cs
@@ -5,8 +5,65 @@ using System.Collections.Generic;
 public class AsteroidCreator : MonoBehaviour {
 
 	public enum AsteroidTypes {s, c, q, x, v, u, r, d, t, m, e, o, p, a};
+
+	public static Dictionary<int, int[]> AsteroidsByRadius = new Dictionary<int, int[]>();
+
+
+	public void InitializeAsteroidsByRadius() {
+		AsteroidsByRadius.Add(8, new int[] {2842, 447, 895, 132, 368, 0, 79, 26, 26, 13, 0, 26, 13, 3});
+		AsteroidsByRadius.Add(9, new int[] {2368, 522, 746, 175, 307, 0, 66, 31, 31, 18, 0, 31, 15, 4});
+		AsteroidsByRadius.Add(10, new int[] {24, 12, 24, 4, 24, 0, 24, 12, 12, 4, 0, 12, 12, 4});
+		AsteroidsByRadius.Add(11, new int[] {18, 15, 18, 4, 18, 0, 18, 15, 15, 4, 0, 15, 15, 4});
+		AsteroidsByRadius.Add(12, new int[] {13, 18, 13, 6, 13, 0, 13, 18, 18, 6, 1, 18, 18, 6});
+		AsteroidsByRadius.Add(13, new int[] {14, 20, 14, 5, 14, 0, 14, 20, 20, 5, 1, 20, 20, 5});
+		AsteroidsByRadius.Add(14, new int[] {3, 29, 3, 5, 3, 0, 3, 29, 29, 5, 2, 29, 29, 5});
+	}
+
 	public static Dictionary<string, string[]> AsteroidCompositions = new Dictionary<string, string[]>();
+
+	public void InitializeAsteroidCompositions() {
+		AsteroidCompositions.Add("s", new string[] {"metal", "olivine", "pyroxene"});
+		AsteroidCompositions.Add("c", new string[] {"silicates", "water", "carbon"});
+		AsteroidCompositions.Add("q", new string[] {"olivine", "pyroxene", "metal"});
+		AsteroidCompositions.Add("x", new string[] {"metal"});
+		AsteroidCompositions.Add("v", new string[] {"pyroxene", "feldspar"});
+		AsteroidCompositions.Add("u", new string[] {"unknown"});
+		AsteroidCompositions.Add("r", new string[] {"pyroxene", "olivine"});
+		AsteroidCompositions.Add("d", new string[] {"silicates", "carbon"});
+		AsteroidCompositions.Add("t", new string[] {"silicates", "carbon"});
+		AsteroidCompositions.Add("m", new string[] {"metal", "enstatite"});
+		AsteroidCompositions.Add("e", new string[] {"enstatite"});
+		AsteroidCompositions.Add("o", new string[] {"silicates", "water", "carbon"});
+		AsteroidCompositions.Add("p", new string[] {"silicates", "carbon"});
+		AsteroidCompositions.Add("a", new string[] {"olivine", "metal"});
+	}
+
+	public string[] GetAsteroidComposition(string key) {
+		return AsteroidCompositions[key];
+	}
+
 	public static Dictionary<string, float[]> AsteroidReflectance = new Dictionary<string, float[]>();
+
+	public void InitializeAsteroidReflectance() {
+		AsteroidReflectance.Add("s", new float[] {0.17f, 0.83f, 0.50f});
+		AsteroidReflectance.Add("c", new float[] {0.33f, 0.50f, 0.50f});
+		AsteroidReflectance.Add("q", new float[] {0.17f, 0.83f, 0.17f});
+		AsteroidReflectance.Add("x", new float[] {0.17f, 0.67f, 0.67f});
+		AsteroidReflectance.Add("v", new float[] {0.00f, 1.00f, 0.00f});
+		AsteroidReflectance.Add("u", new float[] {0.00f, 0.00f, 0.00f});
+		AsteroidReflectance.Add("r", new float[] {0.00f, 0.83f, 0.33f});
+		AsteroidReflectance.Add("d", new float[] {0.33f, 0.67f, 0.83f});
+		AsteroidReflectance.Add("t", new float[] {0.33f, 0.67f, 0.67f});
+		AsteroidReflectance.Add("m", new float[] {0.33f, 0.67f, 0.67f});
+		AsteroidReflectance.Add("e", new float[] {0.33f, 0.50f, 0.67f});
+		AsteroidReflectance.Add("o", new float[] {0.33f, 0.50f, 0.17f});
+		AsteroidReflectance.Add("p", new float[] {0.50f, 0.67f, 0.67f});
+		AsteroidReflectance.Add("a", new float[] {0.17f, 1.00f, 0.83f});
+	}
+
+	public float[] GetAsteroidReflectance(string key) {
+		return AsteroidReflectance[key];
+	}
 
 	/* Attempt to get an Asteroid Type, based on their distribution */
 	public string GetAsteroidType() {
@@ -27,63 +84,50 @@ public class AsteroidCreator : MonoBehaviour {
 		return ((AsteroidTypes)(weights.Length - 1)).ToString ();
 	}
 
-	public string[] GetAsteroidComposition(string key) {
-		return AsteroidCompositions[key];
-	}
+//	public string GetAsteroidTypeByRadius(int radius) {
+//		int[] weights = AsteroidsByRadius[radius];
+//		int sum = 0;
+//		int i;
+//		for (i = 0; i < weights.Length; i++)
+//			sum += weights [i];
+//		int selection = Random.Range (0, sum);
+//		int count = 0;
+//		for (i = 0; i < weights.Length - 1; i++) {
+//			count += weights [i];
+//			if (selection < count)
+//				return ((AsteroidTypes)i).ToString ();
+//		}
+//		return ((AsteroidTypes)(weights.Length - 1)).ToString ();
+//	}
 
-	public float[] GetAsteroidReflectance(string key) {
-		return AsteroidReflectance[key];
-	}
 
 	public GameObject AsteroidTemplate; // a new asteroid sans the mesh
 	public GameObject [] Asteroids; // all possible asteroid meshes
 
 	void Awake() {
-		AsteroidCompositions.Add("s", new string[] {"metal", "olivine", "pyroxene"});
-		AsteroidCompositions.Add("c", new string[] {"silicates", "water", "carbon"});
-		AsteroidCompositions.Add("q", new string[] {"olivine", "pyroxene", "metal"});
-		AsteroidCompositions.Add("x", new string[] {"metal"});
-		AsteroidCompositions.Add("v", new string[] {"pyroxene", "feldspar"});
-		AsteroidCompositions.Add("u", new string[] {"unknown"});
-		AsteroidCompositions.Add("r", new string[] {"pyroxene", "olivine"});
-		AsteroidCompositions.Add("d", new string[] {"silicates", "carbon"});
-		AsteroidCompositions.Add("t", new string[] {"silicates", "carbon"});
-		AsteroidCompositions.Add("m", new string[] {"metal", "enstatite"});
-		AsteroidCompositions.Add("e", new string[] {"enstatite"});
-		AsteroidCompositions.Add("o", new string[] {"silicates", "water", "carbon"});
-		AsteroidCompositions.Add("p", new string[] {"silicates", "carbon"});
-		AsteroidCompositions.Add("a", new string[] {"olivine", "metal"});
-
-		AsteroidReflectance.Add("s", new float[] {0.17f, 0.83f, 0.50f});
-		AsteroidReflectance.Add("c", new float[] {0.33f, 0.50f, 0.50f});
-		AsteroidReflectance.Add("q", new float[] {0.17f, 0.83f, 0.17f});
-		AsteroidReflectance.Add("x", new float[] {0.17f, 0.67f, 0.67f});
-		AsteroidReflectance.Add("v", new float[] {0.00f, 1.00f, 0.00f});
-		AsteroidReflectance.Add("u", new float[] {0.00f, 0.00f, 0.00f});
-		AsteroidReflectance.Add("r", new float[] {0.00f, 0.83f, 0.33f});
-		AsteroidReflectance.Add("d", new float[] {0.33f, 0.67f, 0.83f});
-		AsteroidReflectance.Add("t", new float[] {0.33f, 0.67f, 0.67f});
-		AsteroidReflectance.Add("m", new float[] {0.33f, 0.67f, 0.67f});
-		AsteroidReflectance.Add("e", new float[] {0.33f, 0.50f, 0.67f});
-		AsteroidReflectance.Add("o", new float[] {0.33f, 0.50f, 0.17f});
-		AsteroidReflectance.Add("p", new float[] {0.50f, 0.67f, 0.67f});
-		AsteroidReflectance.Add("a", new float[] {0.17f, 1.00f, 0.83f});
-
-
+		InitializeAsteroidCompositions ();
+		InitializeAsteroidReflectance ();
+		InitializeAsteroidsByRadius ();
 	}
 
 	// Use this for initialization
 	void Start () {
 
-		GameObject asteroidGameObj = GameObject.Instantiate(Asteroids [Random.Range (0, 19)]) as GameObject;
 
-		// create a new asteroid from the template
-		GameObject newAsteroid = GameObject.Instantiate (AsteroidTemplate) as GameObject;
+		for (int i = 0; i < 10; i++) {
+			GameObject asteroidGameObj = GameObject.Instantiate(Asteroids [Random.Range (0, 19)]) as GameObject;
+			
+			// create a new asteroid from the template
+			GameObject newAsteroid = GameObject.Instantiate (AsteroidTemplate) as GameObject;
+			
+			// attach asteroid game object to the newAsteroid
+			asteroidGameObj.transform.parent = newAsteroid.transform;
 
-		// attach asteroid game object to the newAsteroid
-		asteroidGameObj.transform.parent = newAsteroid.transform;
-
-		newAsteroid.transform.localScale = new Vector3 (0.5f, 0.5f, 0.5f);
+			newAsteroid.transform.localScale = new Vector3 (0.5f, 0.5f, 0.5f);
+			newAsteroid.transform.localPosition = new Vector3 (Random.Range(-100,100),
+			                                                   Random.Range(-100,100),
+			                                                   Random.Range(-100,100));
+		}
 
 	}
 	
