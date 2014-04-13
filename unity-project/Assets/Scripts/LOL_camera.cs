5fe0dc2a50c812c0f9e7aad00940a6423d8b0ec4
John Rockefeller <jrockefeller1@gmail.com>
2014-04-13 10:51:21 -0400

    Move camera smoothly between asteroids.

--- unity-project/Assets/Scripts/LOL_camera.cs
+++ unity-project/Assets/Scripts/LOL_camera.cs
@@ -6,68 +6,63 @@ public class LOL_Camera : MonoBehaviour {
 	
 	public static LOL_Camera Instance;
 	public Camera MapCam;
-	void Awake () 
-	{
+
+	private bool isCurrentlyMoving = false;
+	private Vector3 moveToPos;
+
+	void Awake () {
 		Instance = this;
+
+		// set the initial position of the camera to match the moveToPos
+		moveToPos = Camera.main.transform.position;
 	}
 
-	void LateUpdate()
-	{
+	void LateUpdate() {
 		
 		//if (Input.mousePosition.x > 230 && Input.mousePosition.y < 230 && Input.GetMouseButton(0))
-		if ( Input.GetMouseButtonUp(0) && MapCam.pixelRect.Contains(Input.mousePosition))
-		{
+		if (Input.GetMouseButtonUp (0) && MapCam.pixelRect.Contains (Input.mousePosition)) {
 
 			Debug.Log (Input.mousePosition.y + ":" + Input.mousePosition.x);
 			int TerrainLayer = 1 << 10;
+
 			//RaycastHit hitInfo;
-			RaycastHit hitInfo = new RaycastHit();
+			RaycastHit hitInfo = new RaycastHit ();
 
-			if (Physics.Raycast(MapCam.ScreenPointToRay(Input.mousePosition), out hitInfo))
-			{
+			if (Physics.Raycast (MapCam.ScreenPointToRay (Input.mousePosition), out hitInfo)) {
+
 				print (hitInfo.transform.tag);
-			
 
+				Ray ray = MapCam.ScreenPointToRay (Input.mousePosition);
+				Physics.Raycast (ray, out hitInfo, Mathf.Infinity, TerrainLayer);
 
-			Ray ray = MapCam.ScreenPointToRay(Input.mousePosition);
-			Physics.Raycast(ray, out hitInfo, Mathf.Infinity, TerrainLayer);
-			Debug.DrawLine(ray.origin, hitInfo.point);
-			Debug.Log("HitInfo x: " + hitInfo.point.x + " y: " + hitInfo.point.y + " z: " + hitInfo.point.z);
-			
+				Debug.DrawLine (ray.origin, hitInfo.point);
+				Debug.Log ("HitInfo x: " + hitInfo.point.x + " y: " + hitInfo.point.y + " z: " + hitInfo.point.z);
 
+				if (hitInfo.transform.tag != "MapDeadZone") {
 
-
-			//StartCoroutine(	MovePlayer(new Vector3(hitInfo.point.x,1,hitInfo.point.z)));
-			//Debug.Log(hitInfo.transform.tag);
-
-				if (hitInfo.transform.tag != "MapDeadZone"){
-					Camera.main.transform.position = new Vector3(hitInfo.point.x,1,hitInfo.point.z);
 					float currentZ = hitInfo.point.z;
 					float currentX = hitInfo.point.x;//+27.0;
-					//print ()
-					double radius = Math.Sqrt(Math.Pow(currentX,2)+Math.Pow (currentZ,2));
-					double auRad = 2+(radius-45)*(3.5-2.0)/145.0;
-					String auText = "Distance: "+auRad.ToString("0.00")+" AU";
-					GameObject.FindGameObjectWithTag("distanceLabel").GetComponent<TextMesh>().text = auText;
 
-					//print(Mathf.Clamp(10, 1, 3));
+					double radius = Math.Sqrt (Math.Pow (currentX, 2) + Math.Pow (currentZ, 2));
+					double auRad = 2 + (radius - 45) * (3.5 - 2.0) / 145.0;
+					String auText = "Distance: " + auRad.ToString ("0.00") + " AU";
+
+					GameObject.FindGameObjectWithTag ("distanceLabel").GetComponent<TextMesh> ().text = auText;
+
+					// set our new position as the end goal for moving the camera
+					moveToPos = new Vector3 (hitInfo.point.x, 0.0f, hitInfo.point.z);
 				}
 			}
 		}
 
-		
-		
+		if (Camera.main.transform.position != moveToPos) {
+			MoveCamera (moveToPos);
+		}
 	}
 
-	IEnumerator MovePlayer(Vector3 targ){
-		Debug.Log ("moving");
-		float t = 3;
-		while (t > 0) {
-						float step = .1f * Time.deltaTime;
-						Camera.main.transform.position = Vector3.Lerp (Camera.main.transform.position, targ, step);
-						t = t - step;
-						yield return null;
-				}
+	void MoveCamera(Vector3 targetPosition) {
+		float step = 3.0f * Time.deltaTime;
+		Camera.main.transform.position = Vector3.Lerp (Camera.main.transform.position, targetPosition, step);		
 	}
 
 
