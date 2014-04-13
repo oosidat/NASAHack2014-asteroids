232b10a36f7a4fe1c68510372597ef6d7ea6b40b
Osama Sidat <oosidat@gmail.com>
2014-04-13 10:06:35 -0400

    Happily added asteroid attributes based on distance from sun :D. Added SpawnAsteroids method. Cleanup of Asteroidcs

--- unity-project/Assets/Scripts/Asteroid.cs
+++ unity-project/Assets/Scripts/Asteroid.cs
@@ -9,36 +9,21 @@ public class Asteroid : MonoBehaviour {
 	public float vis;
 	public float ir;
 
-	public Asteroid() {
-		AsteroidCreator creator = new AsteroidCreator ();
-		asteroidType = creator.GetAsteroidType ();
-		composition = creator.GetAsteroidComposition(asteroidType);
-		float [] reflectance = creator.GetAsteroidReflectance (asteroidType);
-		uv = reflectance[0];
-		vis = reflectance[1];
-		ir = reflectance[2];
-	}
-
-	/* I think this constructor can and should be used when populating a belt, but I'm not sure... */
-
-	public Asteroid(string type) {
-		AsteroidCreator creator = new AsteroidCreator ();
-		asteroidType = type;
-		composition = creator.GetAsteroidComposition(asteroidType);
-		float [] reflectance = creator.GetAsteroidReflectance (asteroidType);
-		uv = reflectance[0];
-		vis = reflectance[1];
-		ir = reflectance[2];
+	public void SetAsteroidAttributes(string asteroidType, 
+	                                  string[] composition,
+	                                  float uv,
+	                                  float vis,
+	                                  float ir
+	                                  ){
+		this.asteroidType = asteroidType;
+		this.composition = composition;
+		this.uv = uv;
+		this.vis = vis;
+		this.ir = ir;
 	}
 
 	void Start () {
-		AsteroidCreator creator = new AsteroidCreator ();
-		asteroidType = creator.GetAsteroidType ();
-		composition = creator.GetAsteroidComposition(asteroidType);
-		float [] reflectance = creator.GetAsteroidReflectance (asteroidType);
-		uv = reflectance[0];
-		vis = reflectance[1];
-		ir = reflectance[2];
+
 	}
 	
 	// Update is called once per frame
