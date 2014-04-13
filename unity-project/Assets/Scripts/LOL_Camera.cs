791ce1061da2325e04db4128eccdd02fbb45622f
macbook <macbook@wwwerd>
2014-04-12 23:14:43 -0400

    updated interactive map

--- unity-project/Assets/Scripts/LOL_Camera.cs
+++ unity-project/Assets/Scripts/LOL_Camera.cs
@@ -13,15 +13,17 @@ public class LOL_Camera : MonoBehaviour {
 	void LateUpdate()
 	{
 		
-		if (Input.mousePosition.x > 230 && Input.mousePosition.y < 230 && Input.GetMouseButton(0))
+		//if (Input.mousePosition.x > 230 && Input.mousePosition.y < 230 && Input.GetMouseButton(0))
+		if ( Input.GetMouseButton(0))
 		{
 			int TerrainLayer = 1 << 8;
-			RaycastHit hitInfo;
+			//RaycastHit hitInfo;
+			RaycastHit hitInfo = new RaycastHit();
 			Ray ray = MapCam.ScreenPointToRay(Input.mousePosition);
 			Physics.Raycast(ray, out hitInfo, Mathf.Infinity, TerrainLayer);
 			Debug.DrawLine(ray.origin, hitInfo.point);
 			Debug.Log("HitInfo x: " + hitInfo.point.x + " y: " + hitInfo.point.y + " z: " + hitInfo.point.z);
-			
+			//Debug.Log(hitInfo.transform.tag);
 		}
 		
 		
