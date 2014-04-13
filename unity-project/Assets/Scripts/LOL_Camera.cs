762e8871dec61fdd5deec4734535b26a7d47e7c0
macbook <macbook@wwwerd>
2014-04-13 00:45:48 -0400

    dear god please work

--- unity-project/Assets/Scripts/LOL_Camera.cs
+++ unity-project/Assets/Scripts/LOL_Camera.cs
@@ -16,18 +16,40 @@ public class LOL_Camera : MonoBehaviour {
 		//if (Input.mousePosition.x > 230 && Input.mousePosition.y < 230 && Input.GetMouseButton(0))
 		if ( Input.GetMouseButton(0))
 		{
-			int TerrainLayer = 1 << 8;
+			//int TerrainLayer = 1 << 8;
 			//RaycastHit hitInfo;
 			RaycastHit hitInfo = new RaycastHit();
+
+			if (Physics.Raycast(MapCam.ScreenPointToRay(Input.mousePosition), out hitInfo))
+			{
+				print (hitInfo.transform.tag);
+			}
+
+
 			Ray ray = MapCam.ScreenPointToRay(Input.mousePosition);
-			Physics.Raycast(ray, out hitInfo, Mathf.Infinity, TerrainLayer);
+			//Physics.Raycast(ray, out hitInfo, Mathf.Infinity, TerrainLayer);
 			Debug.DrawLine(ray.origin, hitInfo.point);
 			Debug.Log("HitInfo x: " + hitInfo.point.x + " y: " + hitInfo.point.y + " z: " + hitInfo.point.z);
+			StartCoroutine(	MovePlayer(new Vector3(hitInfo.point.x,1,hitInfo.point.z)));
 			//Debug.Log(hitInfo.transform.tag);
+
 		}
 		
 		
 	}
-	
+
+	IEnumerator MovePlayer(Vector3 targ){
+		Debug.Log ("moving");
+		float t = 3;
+		while (t > 0) {
+						float step = .1f * Time.deltaTime;
+						Camera.main.transform.position = Vector3.Lerp (Camera.main.transform.position, targ, step);
+						t = t - step;
+						yield return null;
+				}
+	}
+
+
+
 	
 }
