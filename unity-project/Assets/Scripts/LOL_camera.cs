1310ab964982dd76e92d3545d14beda8e89db693
Osama Sidat <oosidat@gmail.com>
2014-04-14 20:47:29 -0400

    Fixed Fuel Gauge. Will attempt to clean the Lol_Camera mess now. If someone can build and run to test - awesome

--- unity-project/Assets/Scripts/LOL_camera.cs
+++ unity-project/Assets/Scripts/LOL_camera.cs
@@ -6,11 +6,11 @@ public class LOL_Camera : MonoBehaviour {
 	
 	public static LOL_Camera Instance;
 	public Camera MapCam;
-
-	private bool isCurrentlyMoving = false;
-	private Vector3 moveToPos;
 	public float prevx;
 	public float prevz;
+	private bool isCurrentlyMoving = false;
+	private Vector3 moveToPos;
+	
 	void Awake () {
 		Instance = this;
 		prevx = 0;
@@ -18,72 +18,70 @@ public class LOL_Camera : MonoBehaviour {
 		// set the initial position of the camera to match the moveToPos
 		moveToPos = Camera.main.transform.position;
 	}
-
+	
 	void LateUpdate() {
 		
 		//if (Input.mousePosition.x > 230 && Input.mousePosition.y < 230 && Input.GetMouseButton(0))
 		if (Input.GetMouseButtonUp (0) && MapCam.pixelRect.Contains (Input.mousePosition)) {
-
+			
 			Debug.Log (Input.mousePosition.y + ":" + Input.mousePosition.x);
 			int TerrainLayer = 1 << 10;
-
+			
 			//RaycastHit hitInfo;
 			RaycastHit hitInfo = new RaycastHit ();
-
+			
 			if (Physics.Raycast (MapCam.ScreenPointToRay (Input.mousePosition), out hitInfo)) {
-
+				
 				print (hitInfo.transform.tag);
-
+				
 				Ray ray = MapCam.ScreenPointToRay (Input.mousePosition);
 				Physics.Raycast (ray, out hitInfo, Mathf.Infinity, TerrainLayer);
-
+				
 				Debug.DrawLine (ray.origin, hitInfo.point);
 				Debug.Log ("HitInfo x: " + hitInfo.point.x + " y: " + hitInfo.point.y + " z: " + hitInfo.point.z);
-
+				
 				if (hitInfo.transform.tag != "MapDeadZone") {
-
+					
 					float currentZ = hitInfo.point.z;
 					float currentX = hitInfo.point.x;//+27.0;
-
+					
 					double radius = Math.Sqrt (Math.Pow (currentX, 2) + Math.Pow (currentZ, 2));
 					double auRad = 2 + (radius - 45) * (3.5 - 2.0) / 145.0;
-					double magDifference = Math.Sqrt(Math.Pow (currentX-prevx,2)+Math.Pow(currentZ-prevz,2));
-
 					String auText = "Distance: " + auRad.ToString ("0.00") + " AU";
-
+					double magDifference = Math.Sqrt(Math.Pow (currentX-prevx,2)+Math.Pow (currentZ-prevz,2));
 					GameObject.FindGameObjectWithTag ("distanceLabel").GetComponent<TextMesh> ().text = auText;
-
+					print("mag difference: "+magDifference);
 					// set our new position as the end goal for moving the camera
 					moveToPos = new Vector3 (hitInfo.point.x, 0.0f, hitInfo.point.z);
 					float fuelExpended = (float)magDifference*0.0005f;
 					Controls control = GameObject.Find ("Player").GetComponent<Controls>();
 					control.currentFuel = control.currentFuel-(float)fuelExpended;
-					print ("fuel expended: "+fuelExpended+"current fuel: "+control.currentFuel );
-					fuelGage fuelg = GameObject.Find ("FuelGage").GetComponent<fuelGage>();
+					int intfuel = (int)Math.Floor(10.0f*control.currentFuel);
+					print ("fuel expended: "+fuelExpended+"current fuel: "+control.currentFuel+" fuel int: "+intfuel );
+					fuelGage fuelg = GameObject.Find ("FuelCell").GetComponent<fuelGage>();
 					float hundredfuel = 100.0f*control.currentFuel;
 					String fuelText = "Current Charge: "+hundredfuel.ToString("0.0");
-					int intfuel = (int)Math.Floor(control.currentFuel);
-					GameObject.FindGameObjectWithTag ("fuelLabel").GetComponent<TextMesh> ().text = fuelText;
-
-
+					//GameObject.FindGameObjectWithTag ("fuelLabel").GetComponent<TextMesh> ().text = auText;
+					
+					
 					fuelg.changeTexture(intfuel);
 					prevx = currentX;
 					prevz = currentZ;
 				}
 			}
 		}
-
+		
 		if (Camera.main.transform.position != moveToPos) {
 			MoveCamera (moveToPos);
 		}
 	}
-
+	
 	void MoveCamera(Vector3 targetPosition) {
 		float step = 3.0f * Time.deltaTime;
 		Camera.main.transform.position = Vector3.Lerp (Camera.main.transform.position, targetPosition, step);		
 	}
-
-
+	
+	
 	/*
         if (player.hud.MiniMap.IsInViewport(Input.mousePosition))
         {
@@ -113,7 +111,7 @@ public class LOL_Camera : MonoBehaviour {
             }                
         }
 	 */
-
-
+	
+	
 	
 }
