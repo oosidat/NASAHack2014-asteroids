0cb9e9df038aa0a0df11c613dbc5f1dd08743f8d
Osama Sidat <oosidat@gmail.com>
2014-04-16 22:37:30 -0400

    Added google analytics script; currently tied to oosidat@gmail.com

--- WebView/scripts/addscript.js
+++
@@ -1,4 +0,0 @@
-var unityObjectUrl = "UnityObject2.js";
-if (document.location.protocol == 'https:')
-	unityObjectUrl = unityObjectUrl.replace("http://", "https://ssl-");
-document.write('<script type="text\/javascript" src="' + unityObjectUrl + '"><\/script>');
