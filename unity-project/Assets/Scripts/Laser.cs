e8a04d7dbb7a97c94745a49e3656532ef9815f20
rich.windows <rich@windows.local>
2014-04-18 21:24:19 -0400

    updated laser script

--- unity-project/Assets/Scripts/Laser.cs
+++ unity-project/Assets/Scripts/Laser.cs
@@ -2,14 +2,24 @@
 using System.Collections;
 
 public class Laser : MonoBehaviour {
+	Color c1 = Color.yellow;
+	Color c2 = Color.red;
+	LineRenderer lineRenderer;
 
 	// Use this for initialization
 	void Start () {
-	
+		lineRenderer = gameObject.AddComponent<LineRenderer>();
+		lineRenderer.material = new Material (Shader.Find("Particles/Additive"));
+		lineRenderer.SetColors(c1, c2);
+		lineRenderer.SetWidth(.2f,.2f);
+		lineRenderer.SetVertexCount(2);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		lineRenderer.SetPosition(0, new Vector3(transform.position.x, transform.position.y - 2, transform.position.z));
+		lineRenderer.SetPosition(1, hitInfo.point);
+		lineRenderer.SetPosition(2, new Vector3(transform.position.x, transform.position.y - 2, transform.position.z));
 	}
 }
