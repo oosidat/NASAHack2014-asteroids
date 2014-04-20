909534b6dc7d588207fce0f3d35a4b755aeb5f04
rich.windows <rich@windows.local>
2014-04-19 21:12:49 -0400

    step one of gameover/highscores

--- unity-project/Assets/fuelGage.cs
+++ unity-project/Assets/fuelGage.cs
@@ -19,12 +19,16 @@ public class fuelGage : MonoBehaviour {
 
 		if (value <= 0) {
 			value = 0;
+			Controls controls = GameObject.Find ("Player").GetComponent<Controls>();
+			float score = controls.currentMoney;
+			Debug.Log("score: " + score);
 			GameObject.Find ("GameOver").renderer.enabled=true;
 			GameObject.Find ("GameOver").collider.enabled=true;
-			GameObject.Find ("GameOverText").GetComponent<TextMesh>().text="Game Over";
+			GameObject.Find ("GameOverText").GetComponent<TextMesh>().text="Congratulations!\n\nYou mined $"+score.ToString("0.00");;
 			GameObject.Find ("GameOverText").renderer.enabled=true;
 
-				}
+
+		}
 		renderer.material.mainTexture = images[value];
 
 
